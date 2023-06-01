30fcf02e3727abc4f9dec7cbb17b77212a5a68ba
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-06-01 20:09:30 +0300

    added animations + hotfix

--- lib/login_system_tutorial.dart
+++ lib/login_system_tutorial.dart
@@ -7,6 +7,10 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+// ignore: unused_import
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 
 class TutorialLoginSystem extends StatelessWidget {
   const TutorialLoginSystem({super.key, this.onTap});
@@ -20,74 +24,98 @@ class TutorialLoginSystem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              const SizedBox(height: 50),
-
-              // logo
-              Icon(Icons.lock, color: Colors.black, size: 100,),
-
-              const SizedBox(height: 25),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                const SizedBox(height: 20),
       
-              // wlcm
-              _welcome(),
-
-                const SizedBox(height: 50,),
+                // logo
+                // Icon(Icons.lock, color: Colors.black, size: 100,),
+                // AnimatedIcon(icon: Icons., progress: progress),
+                Lottie.network("https://assets3.lottiefiles.com/packages/lf20_xhyszish.json", animate: null, width: 150),
+                
       
-              // nick
-
-              _userName(),
-
-              const SizedBox(height: 10),
-
-              // passwd
-              _passwd(),
+                const SizedBox(height: 25),
+        
+                // wlcm
+                _welcome(),
+      
+                  const SizedBox(height: 50,),
+        
+                // nick
+      
+                _userName(),
+      
+                const SizedBox(height: 10),
+      
+                // passwd
+                _passwd(),
+                
+                const SizedBox(height: 10,),
+        
+                // reset passwd?
+                _resetPasswd(),
+      
+                const SizedBox(height: 25,),
+                // sign
+                GestureDetector(
+                  onTap: userSignIn,
+                  child: _signInButton(),
+                ),
+                
+                const SizedBox(height: 50),
+        
+                // google apple auth
+                _orLoginWith(),
+      
+                SizedBox(height: 50,),
+      
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // google
+                    Image.network(
+                      "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png",
+                      height: 40,
+                    ),
+                    SizedBox(width: 50),
+                    // apple
+                    Image.network("https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png",
+                      height: 40,
+                    )
+                  ],
+                ),
+                SizedBox(height: 60),
+      
+                _signUp()
               
-              const SizedBox(height: 10,),
-      
-              // reset passwd?
-              _resetPasswd(),
-
-              const SizedBox(height: 25,),
-              // sign
-              GestureDetector(
-                onTap: userSignIn,
-                child: _signInButton(),
-              ),
-              
-              const SizedBox(height: 50),
-      
-              // google apple auth
-              _orLoginWith(),
-
-              SizedBox(height: 50,),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // google
-                  Image.network(
-                    "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png",
-                    height: 40,
-                  ),
-                  SizedBox(width: 50),
-                  // apple
-                  Image.network("https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png",
-                    height: 40,
-                  )
-                ],
-              ),
-              SizedBox(height: 60),
-
-              _signUp()
-            ],
+                
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class _welcome extends StatelessWidget {
+  const _welcome({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedTextKit(
+      totalRepeatCount: 1, 
+      animatedTexts: [TypewriterAnimatedText("Добро пожаловать, рад вас видеть снова!",
+      textStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+        ))]);
   }
 }
 
@@ -103,30 +131,19 @@ class _signUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      // mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("У вас аккаунта нет?", style: TextStyle(color: Colors.black),),
-        SizedBox(width: 4),
-        Text("Зарегистрируйтесь!", style: TextStyle(color: Colors.green.shade800, fontWeight: FontWeight.bold),)
+        Padding(
+          padding: const EdgeInsets.only(left: 50),
+          child: Text("У вас аккаунта нет?",style: TextStyle(color: Colors.black),),
+        ),
+        SizedBox(width: 4,),
+        AnimatedTextKit(animatedTexts: [
+          FadeAnimatedText("Зарегистрируйтесь!", 
+          textStyle: TextStyle(
+            color: Colors.green.shade400, fontWeight: FontWeight.bold))])
       ],
     );
-  }
-}
-
-class _welcome extends StatelessWidget {
-  const _welcome({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "Добро пожаловать, рад вас видеть снова!",
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 17
-      ),
-      );
   }
 }
 
@@ -149,7 +166,11 @@ class _orLoginWith extends StatelessWidget {
 
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text("Или войти через", style: TextStyle(color: Colors.black, fontSize: 15)),
+            child: 
+            // AnimatedTextKit(animatedTexts: 
+            // [FadeAnimatedText("Или войти через", 
+            // textStyle: TextStyle(color: Colors.black, fontSize: 15))])
+            Text("Или войти через", style: TextStyle(color: Colors.black, fontSize: 15)),
           ),
 
           Expanded(
